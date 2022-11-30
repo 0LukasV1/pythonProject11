92538f8a796670579a1012d9b314fbfc7aeeb2c4
0LukasV1 <113472942+0LukasV1@users.noreply.github.com>
2022-11-30 09:27:06 +0100

    Add files via upload

--- prva uloha pocitanie pismenok.py
+++ prva uloha pocitanie pismenok.py
@@ -1,1 +1,13 @@
 fr = open('data/udaje.txt','r', encoding ="utf-8")
+
+slovnik ={}
+ms = fr.read()
+konriad = 0
+for znak in ms:
+    if znak.isupper() or znak.islower():
+        slovnik[znak]= slovnik.get(znak,0) + 1
+    if znak == '\n':
+        konriad +=1
+
+print(slovnik)
+print(konriad)
